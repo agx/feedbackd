efd088daa8d99eeb80679fef1dd66770aad99425
Guido Günther <agx@sigxcpu.org>
2024-06-12 08:47:42 +0200

    feedback-led: Document hw LED types

--- src/fbd-feedback-led.h
+++ src/fbd-feedback-led.h
@@ -9,6 +9,16 @@
 
 G_BEGIN_DECLS
 
+/**
+ * FbdDevLedColor:
+ * @FBD_FEEDBACK_LED_COLOR_WHITE: A white LED
+ * @FBD_FEEDBACK_LED_COLOR_RED: A red LED
+ * @FBD_FEEDBACK_LED_COLOR_GREEN: A green LED
+ * @FBD_FEEDBACK_LED_COLOR_BLUE: A blue LED
+ * @FBD_FEEDBACK_LED_COLOR_RGB: A multicolor LED that can display arbitrary RGB values
+ *
+ * The color of a hardware LED.
+ */
 typedef enum _FbdFeedbackLedColor {
   FBD_FEEDBACK_LED_COLOR_WHITE = 0,
   FBD_FEEDBACK_LED_COLOR_RED = 1,
